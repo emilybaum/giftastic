0342ac31e319281a7d020fae67294031d89bc202
emilybaum <emily.r.baum@gmail.com>
2019-05-23 21:36:38 -0400

    added api call but the contnet is not showing up on the page yet

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -7,7 +7,7 @@ $(document).ready( function() {
         $("#button-holder").empty();
         for (var i = 0; i < topics.length; i++) {
             var button = $("<button>");
-            button.addClass("gifs");
+            button.addClass("gif-button");
             button.attr("gif-topic", topics[i]);
             button.text(topics[i]);
             $("#button-holder").append(button);
@@ -18,12 +18,41 @@ $(document).ready( function() {
     // ADD NEW GIF TO THE ARRAY OF GIFS
     $("#add-gif").on("click", function (event) {
         event.preventDefault();
-        var newGif = $("#search-input").val().trim();
-        topics.push(newGif);
+        var newTopic = $("#search-input").val().trim();
+        topics.push(newTopic);
         createButtons();
     })
     
+    // LISTEN FOR BUTTON CLICK
+    $(document).on("click", ".gif-button", makeGif)
 
+    function makeGif() {
 
+        var whatToSearch = $(this).attr("gif-topic")
+        var API = "HxkLNilC8OgbgenMW1pjUSt5JOV4ynGe";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + API + "&q=" + whatToSearch + "&limit=10&offset=0&rating=PG-13&lang=en";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function(response) {
+            console.log(response)
+            // $("#gif-display").empty();
+            $("#gif-display").text(JSON.stringify(response));
+
+            // for (var i = 0; i < response.data.length; i++) {
+            //     var newGif = $("<img>");
+            //     newGif.addClass("gif-generator");
+            //     newGif.text(response.data[i].images.fixed_width.url)
+            //     $("#gif-display").append(newGif);
+            // }
+            
+
+        })
+
+    }
 
 })
+
+
+// use for testin: https://api.giphy.com/v1/gifs/search?api_key=HxkLNilC8OgbgenMW1pjUSt5JOV4ynGe&q=tacos&limit=25&offset=0&rating=PG-13&lang=en
